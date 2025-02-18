7f5cbe00ab2fd7dd55fa1bf5f5cf85f47beb15c1
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-02-18 16:23:00 +0100

    feat: export sanctus fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,30 +5,32 @@ export default defineConfig({
     build: {
         sourcemap: true,
         lib: {
-            entry: "src/sanctus.ts",
+            entry: "./src/sanctus.ts",
             name: "Sanctus",
+            fileName: (format) => `sanctus.${format}.js`,
+            formats: ["es", "cjs", "umd"]
         },
-        rollupOptions: {
-            output: [
-                {
-                    format: "es",
-                    entryFileNames: "index.esm.js",
-                    dir: "dist",
-                },
-                {
-                    format: "cjs",
-                    entryFileNames: "index.cjs.js",
-                    dir: "dist",
-                },
-                {
-                    format: "umd",
-                    entryFileNames: "index.umd.js",
-                    dir: "dist",
-                    name: "Sanctus", // Global variable for browsers
-                    sourcemap: true,
-                },
-            ],
-        },
+        // rollupOptions: {
+        //     output: [
+        //         {
+        //             format: "es",
+        //             entryFileNames: "index.esm.js",
+        //             dir: "dist",
+        //         },
+        //         {
+        //             format: "cjs",
+        //             entryFileNames: "index.cjs.js",
+        //             dir: "dist",
+        //         },
+        //         {
+        //             format: "umd",
+        //             entryFileNames: "index.umd.js",
+        //             dir: "dist",
+        //             name: "Sanctus", // Global variable for browsers
+        //             sourcemap: true,
+        //         },
+        //     ],
+        // },
     },
     plugins: [dts()],
 });
